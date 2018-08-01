4a035a063729379d8a5ce43ba08501ad2304d468
EhrmannS <steffen@funroll-loops.de>
2018-08-01 17:17:42 +0300

    test errors

--- tests/testthat/test_oCLC.R
+++ tests/testthat/test_oCLC.R
@@ -1,7 +1,15 @@
 library(checkmate)
+library(magrittr)
 context("oCLC")
 
 
-test_that("make tests", {
-
+test_that("Error if arguments have wrong value", {
+  myMask <- loadData(files = "aWindow.csv",
+                     localPath = system.file("csv", package="rasterTools")) %>%
+    geomRectangle() %>%
+    setCRS(crs = projs$laea)
+  
+ expect_error(oCLC(mask = myMask))
+ expect_error(oCLC(mask = myMask, years = "bla"))
+ 
 })
