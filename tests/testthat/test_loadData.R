9e23451978c2f779535601b97063e6424e681909
EhrmannS <steffen@funroll-loops.de>
2018-08-01 17:17:27 +0300

    test kml files

--- tests/testthat/test_loadData.R
+++ tests/testthat/test_loadData.R
@@ -3,15 +3,21 @@ context("loadData")
 
 
 test_that("function loads 'csv' files", {
-  output <- loadData(files = "locations.csv",
-                     localPath = system.file("csv", package="rasterTools"))
-  expect_class(output, "geom")
+  output <- loadData(files = c("aWindow.csv", "locations.csv"),
+                     localPath = system.file("csv", package="rasterTools"),
+                     verbose = TRUE)
+  expect_class(output, "list")
+  expect_class(output[[1]], "geom")
 })
 
 test_that("function loads 'kml' files", {
-
+  output <- loadData(files = "cgrs_estonia.kml",
+                     localPath = system.file("kml", package="rasterTools"))
+  expect_class(output, "SpatialPolygonsDataFrame")
 })
 
-test_that("function loads '' files", {
-
+test_that("function loads 'tif' files", {
+  # output <- loadData(files = "cgrs_estonia.kml",
+  #                    localPath = system.file("tif", package="rasterTools"))
+  # expect_class(output, "raster")
 })
