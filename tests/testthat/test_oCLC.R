bc7bc7dedca75c38e96d3c827ce7e0dd453c963c
EhrmannS <steffen@funroll-loops.de>
2018-08-01 19:15:59 +0300

    updates

--- tests/testthat/test_oCLC.R
+++ tests/testthat/test_oCLC.R
@@ -1,7 +1,20 @@
 library(checkmate)
+library(testthat)
 library(magrittr)
+library(raster)
 context("oCLC")
 
+
+test_that("oCLC loads the correct file", {
+  updatePaths(root = system.file("test_datasets", package="rasterTools"))
+  myMask <- loadData(files = "aWindow.csv",
+                     localPath = system.file("csv", package="rasterTools")) %>%
+    geomRectangle() %>%
+    setCRS(crs = projs$laea)
+  
+  output <- oCLC(mask = myMask, years = 2000)
+  expect_class(output, "RasterStack")
+})
 
 test_that("Error if arguments have wrong value", {
   myMask <- loadData(files = "aWindow.csv",
@@ -11,5 +24,6 @@ test_that("Error if arguments have wrong value", {
   
   expect_error(oCLC(mask = "myMask"))
   expect_error(oCLC(mask = myMask, years = "bla"))
- 
+  expect_error(oCLC(mask = myMask, years = 2001))
+  
 })
--- tests/testthat/test_oMODIS.R
+++ tests/testthat/test_oMODIS.R
@@ -1,4 +1,5 @@
 library(checkmate)
+library(testthat)
 context("oMODIS")
 
 
