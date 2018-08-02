d7c0d7e0e7ce76b55bb85bc558aa0d0c61a0e9e2
EhrmannS <steffen@funroll-loops.de>
2018-08-02 13:55:15 +0300

    complete testing of oCLC

--- tests/testthat/test_oCLC.R
+++ tests/testthat/test_oCLC.R
@@ -16,6 +16,18 @@ test_that("oCLC loads the correct file", {
   expect_class(output, "RasterStack")
 })
 
+test_that(("oCLC works with Spatial* mask (that has another crs than the dataset)"), {
+  myMask <- loadData(files = "aWindow.csv",
+                     localPath = system.file("csv", package="rasterTools")) %>%
+    geomRectangle() %>%
+    setCRS(crs = projs$laea)
+  myMask <- gToSp(geom = myMask, crs = projs$laea) %>% 
+    setCRS(crs = projs$longlat)
+  
+  output <- oCLC(mask = myMask, years = 2000)
+  expect_class(output, "RasterStack")
+})
+
 test_that("Error if arguments have wrong value", {
   myMask <- loadData(files = "aWindow.csv",
                      localPath = system.file("csv", package="rasterTools")) %>%
