51c2b96b47aa2b1e8b02eab48f98406162099d51
EhrmannS <steffen@funroll-loops.de>
2018-08-17 10:53:06 +0300

    getTable for RasterLayer

--- tests/testthat/test_getTable.R
+++ tests/testthat/test_getTable.R
@@ -4,7 +4,7 @@ library(raster)
 context("getTable")
 
 
-test_that("getTable of a geom", {
+test_that("getTable of a 'geom'", {
   coords <- data.frame(x = c(40, 70, 70, 50),
                        y = c(40, 40, 60, 70),
                        id = 1)
@@ -15,4 +15,24 @@ test_that("getTable of a geom", {
   
   expect_data_frame(output, any.missing = FALSE, nrows = 4, ncols = 3)
   expect_names(names(output), identical.to = c("x", "y", "id"))
+})
+
+test_that("getTable of a 'RasterLayer'", {
+  input <- rtData$continuous
+  
+  # test RasterLayer without attribute table
+  output <- getTable(input)
+  expect_data_frame(output, any.missing = FALSE, nrows = 0, ncols = 0)
+
+  # test RasterLayer with attribute table
+  r <- raster(nrow=10, ncol=10)
+  r[] <- 1; r[51:100] <- 2; r[3:6, 1:5] <- 3
+  r <- ratify(r)
+  rat <- raster::levels(r)[[1]]
+  rat$landcover <- c('Pine', 'Oak', 'Meadow')
+  rat$code <- c(12,25,30)
+  levels(r) <- rat
+  output <- getTable(r)
+  expect_data_frame(output, any.missing = FALSE, nrows = 3, ncols = 3)
+  expect_names(names(output), identical.to = c("ID", "landcover", "code"))
 })
