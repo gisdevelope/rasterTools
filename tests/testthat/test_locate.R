947fc77a8820bee6227ffc3abc0ffc486e5af1bf
Steffen Ehrmann <steffen.science@funroll-loops.de>
2018-09-14 00:30:06 +0200

    more tests

--- tests/testthat/test_locate.R
+++ tests/testthat/test_locate.R
@@ -1,7 +1,11 @@
 library(checkmate)
+library(testthat)
 context("locate")
 
 
 test_that("make tests", {
-
+  input <- rtData$continuous
+  visualise(input)
+  output <- locate(1)
+  expect_data_frame(output)
 })
--- tests/testthat/test_oWCLIM.R
+++ tests/testthat/test_oWCLIM.R
@@ -4,26 +4,26 @@ context("oWCLIM")
 
 
 test_that("oWCLIM loads the correct file", {
-  # setPaths(root = system.file("test_datasets", package="rasterTools"))
-  # myMask <- loadData(files = "aWindow.csv",
-  #                    localPath = system.file("csv", package="rasterTools")) %>%
-  #   geomRectangle() %>%
-  #   setCRS(crs = projs$laea)
-  # 
-  # output <- oWCLIM(mask = myMask, )
-  # expect_class(output, "RasterStack")
+  setPaths(root = system.file("test_datasets", package="rasterTools"))
+  myMask <- geomRectangle(data.frame(x = c(5093616, 5103222),
+                                     y = c(4054188, 4064870))) %>%
+    setCRS(crs = projs$laea)
+  
+  output <- oWCLIM(mask = myMask, variable = c("tmean"), month = 5)
+  expect_list(output, len = 1)
+  expect_class(output$tavg, "RasterStack")
 })
 
 test_that(("oWCLIM works with Spatial* mask (that has another crs than the dataset)"), {
-  # myMask <- loadData(files = "aWindow.csv",
-  #                    localPath = system.file("csv", package="rasterTools")) %>%
-  #   geomRectangle() %>%
-  #   setCRS(crs = projs$laea)
-  # myMask <- gToSp(geom = myMask) %>% 
-  #   setCRS(crs = projs$longlat)
-  # 
-  # output <- oWCLIM(mask = myMask, )
-  # expect_class(output, "RasterStack")
+  myMask <- geomRectangle(data.frame(x = c(5093616, 5103222),
+                                     y = c(4054188, 4064870))) %>%
+    setCRS(crs = projs$laea)
+  myMask <- gToSp(geom = myMask) %>%
+    setCRS(crs = projs$longlat)
+
+  output <- oWCLIM(mask = myMask, variable = c("tmean"), month = 5)
+  expect_list(output, len = 1)
+  expect_class(output$tavg, "RasterStack")
 })
 
 test_that("Error if arguments have wrong value", {
@@ -40,14 +40,13 @@ test_that("Error if arguments have wrong value", {
 })
 
 test_that("bibliography item has been created", {
-  # myMask <- loadData(files = "aWindow.csv",
-  #                    localPath = system.file("csv", package="rasterTools")) %>%
-  #   geomRectangle() %>%
-  #   setCRS(crs = projs$laea)
-  # options(bibliography = NULL)
-  # 
-  # output <- oWCLIM(mask = myMask, )
-  # theBib <- getOption("bibliography")
-  # expect_class(theBib, classes =  "bibentry")
-  # expect_list(theBib, len = 1)
+  setPaths(root = system.file("test_datasets", package="rasterTools"))
+  myMask <- geomRectangle(data.frame(x = c(5093616, 5103222),
+                                     y = c(4054188, 4064870))) %>%
+    setCRS(crs = projs$laea)
+  
+  output <- oWCLIM(mask = myMask, variable = c("tmean"), month = 5)
+  theBib <- getOption("bibliography")
+  expect_class(theBib, classes =  "bibentry")
+  expect_list(theBib, len = 1)
 })
