f8e3ca72a93c191302f87b070f815a5cc51b2bd4
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2019-01-16 18:35:43 +0100

    update documentation

--- man-roxygen/kernel2.R
+++ man-roxygen/kernel2.R
@@ -1,4 +1,4 @@
-#' @param kernel [\code{matrix(., .)} | \code{list(.)} thereof]\cr scan the
+#' @param kernel [\code{matrix(.)} | \code{list(.)} thereof]\cr scan the
 #'   raster with this kernel (default is a 3 by 3 cells diamond kernel).
 #' @details The cells of a kernel can have the values 0, 1 and NA. The kernel
 #'   cells with values 0 and 1 are matched accurately in the input raster. The
